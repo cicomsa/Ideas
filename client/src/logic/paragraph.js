ad86740cb28c84be8a5d32b35edfc388399e6bc3
cicomsa <ioanacomsa85@gmail.com>
2018-06-13 01:36:36 +0200

    paragraph version 2

--- client/src/logic/paragraph.js
+++ client/src/logic/paragraph.js
@@ -4,21 +4,28 @@ let sentence = "Another! Yesterday. Today! Tomorrow. Hello, another. me!"
 
 const paragraph = (sentence) => {
   
-    
+    let result = []
     let splitText = sentence.split('. ')
+
     //find what to work with 
     let includesSign = splitText.filter(text=>(text.includes('!') ))
+   
+    let signLastChar = includesSign.filter(text=> text.charAt(text.length-1) === '!' ).join('')
+    
+    let signMiddleChar = includesSign.filter(text=> text.charAt(text.length-1) !== '!')
 
-    let signLastChar = includesSign.filter(text=> text.charAt(text.length-1) === '!')
+    let splitSignMiddleChar = () => {
+        if (signMiddleChar)  {
+            let splitSign = signMiddleChar.map(text=>text.split('!'))
+            let result = splitSign.map(text=>text[0]+ '!')
+            return result.join('')
+            }
+    
+    } 
 
-    if (signLastChar) {
-        return includesSign 
-    } else  {
-        let splitSign = includesSignToFilter.map(text=>text.split('!'))
-        let result = splitSign.map(text=>text[0]+ '!')
-        return result
-    }
-        
+    splitSignMiddleChar() ? result.push(splitSignMiddleChar(), signLastChar): result.push(signLastChar)
+    
+    return result  
 
    
 
--- client/src/logic/paragraph.test.js
+++ client/src/logic/paragraph.test.js
@@ -2,7 +2,15 @@ const paragraph = require('./paragraph')
 
 let sentence = "So, I thought about something today. And what I saw was that I don't have that content in me. That type of life that my eyes have. My eyes have the life already! They are already happy!"
 
-test('sentence contains !', () => {
+let sentence1 = "So! I thought about something today. And what I saw was that. I don't have that content in me. That type of life that my eyes have. My eyes have the life already! They are already happy!"
+
+test('sentence contains', () => {
     expect(paragraph(sentence)).toEqual([ "My eyes have the life already! They are already happy!"]
 );
 });
+
+test('sentence1 contains', () => {
+    expect(paragraph(sentence1)).toEqual(["So!",  "My eyes have the life already! They are already happy!"]
+);
+});
+
