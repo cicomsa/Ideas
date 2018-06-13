878a60378fe5b4333153c05efae88379b98db73a
cicomsa <ioanacomsa85@gmail.com>
2018-06-13 02:17:22 +0200

    endWith version 2

--- client/src/logic/endWith.js
+++ client/src/logic/endWith.js
@@ -1,15 +1,12 @@
 //version 8
 const endWith = (sentence) => {
-
-    let signAtChar = sentence.charAt(sentence.length -1) === ('!') && !sentence.includes('. ')
-
-    if (signAtChar)  {
-        return signAtChar 
-     } else { 
-        let text = sentence.split('. ')
-        return text.filter(text=>text.includes('!')).join('')
+    if (!sentence.includes('. ')) {
+        return sentence
+    } else {
+        let splitText = sentence.split('. ')
+        let filteredText =  splitText.filter(text=>text.includes('!')).join('')
+        return filteredText             
     }
-
 }
 
 module.exports = endWith
--- client/src/logic/endWith.test.js
+++ client/src/logic/endWith.test.js
@@ -2,6 +2,12 @@ const endWith = require('./endWith')
 
 let sentence = "Yesterday. Today!"
 
+let sentence1 = "Yesterday! Today!"
+
 test('sentence contains !', () => {
     expect(endWith(sentence)).toBe("Today!");
+});
+
+test('sentence contains', () => {
+    expect(endWith(sentence1)).toBe("Yesterday! Today!");
 });
